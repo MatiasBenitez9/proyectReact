93a12961413fe36e1c0ce7362d5f15d9bb16f0cc
MatiasBenitez9 <msbenitez98@gmail.com>
2024-04-09 19:42:07 -0300

    otros

--- React1/src/components/Item.jsx
+++ React1/src/components/Item.jsx
@@ -11,7 +11,9 @@ export const Item = ( { product } ) =>  (
               <Card.Title>{ product.name }</Card.Title>
               <Card.Text>{ product.detail }</Card.Text>
               <Card.Text>{ product.category }</Card.Text>
-              <Link to={"/item/${product.id}" }><Button variant="primary">Comprar</Button></Link>
+              <Link to={"/item/${product.id}" }>
+                <Button variant="primary">Comprar</Button>
+              </Link>
             </Card.Body>
           </Card>
         );
